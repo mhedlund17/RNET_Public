342bfebc283ae3107d7f3ed39192c8ee77f77e6c
mhedlund17 <mhedlund@stanford.edu>
2025-10-28 04:15:51 -0700

    Create CCDT_feature_details.m

---
+++ CCDT/Human_Analysis/CCDT_feature_details.m
@@ -0,0 +1,80 @@
+%% save details about selected features
+% fPv --> mean z-scored value of selected power features over fastest third of trials
+% fIDp --> fband of each selected power feature
+% ssIDp --> subject/session number of selected power feature
+% sPv --> mean z-scored value of selected power features over slowest third of trials
+% aPv --> mean z-scored value of selected power features over all trials
+% fQv, fIDq, ssIDq, sQv, aQv --> same variable meanings, but for qexp features
+
+% percent threshold: features with a significant regression slope in
+% more than this percentage of bootstrap iterations will be included in
+% the selected feature space. The threshold used for analysis in paper was
+% chosen by evaluating SVM performance in predicting fast vs slow trials
+% for 20 thresholds between 0 and 100%. 
+percThresh = .3; % percentage, as a decimal
+savedir2 = ''; %output directory
+savefname2 = ''; %output file name
+
+% identify features above selection threshold 
+for isubj = 1:Nsubj
+    sigNodesPow{isubj,1} = (sum(sigPow{isubj,1},3)/1000 > percThresh);
+    sigNodesCom{isubj,1} = (sum(sigCom{isubj,1},3)/1000 > percThresh);
+end
+
+% initiate empty variables
+fPv = []; sPv = []; %average power feature value over fast and slow  trials
+fIDp = []; ssIDp = []; %frequency band and subjectID of power features
+fQv = []; sQv = []; %fast and slow communicability features
+aPv = []; aQv = []; %all power and communicability features
+fIDq = []; ssIDq = []; %frequency band of communicability features
+gchP = []; gchQ = []; %channel labels of power and communicability features
+for isubj = 1:Nsubj
+    disp(['Subject: ' num2str(isubj)])
+
+    % get iF, iS - trial indices for fastest and slowest third of trials (by RT)
+    RT = behavStruct(isubj).vRT;
+    iF = behavStruct(isubj).ifast;
+    iS = behavStruct(isubj).islow;
+
+    %get features for current subject
+    sigFeatP = sigNodesPow{isubj}; %nElec x nFreq
+    sigFeatQ = sigNodesCom{isubj};
+    nTrl = size(nPowDat,2);
+    nFreq = size(nPowDat,3);
+
+    for j = 1:nFreq %fbands, reformat features for current subject
+        nComDat(:,:,j) = NFstructQ(isubj).fbands(j).NFqexp_nodal;
+        nPowDat(:,:,j) = NFstructP(isubj).fbands(j).NFpow;
+    end
+
+    %get selected features
+    for i = 1:nFreq
+        %power features
+        powFeaturesZ = zscore(nPowDat(:,:,i),1,'all'); % zscore over all channels/trials
+        sigPowFeaturesZ = powFeaturesZ(sigFeatP(:,i),:); % get just selected channels
+        fPv = vertcat(fPv,mean(sigPowFeaturesZ(:,iF),2)); %average over fast trials
+        sPv = vertcat(sPv,mean(sigPowFeaturesZ(:,iS),2)); %average over slow trials
+        aPv = vertcat(aPv,mean(sigPowFeaturesZ(:,:),2)); %average over all trials
+        if ~isempty(sigPowFeaturesZ)
+            fIDp = vertcat(fIDp,i*ones(height(sigPowFeaturesZ),1)); % frequency band
+            ssIDp = vertcat(ssIDp,isubj*ones(height(sigPowFeaturesZ),1)); % subject ID
+            gchP = vertcat(gchP,NFstructP(isubj).gchlbl(sigFeatP(:,i))); % channel label
+        end
+        %communicability features
+        comFeaturesZ = zscore(nComDat(:,:,i),1,'all'); % zscore over all channels/trials
+        sigComFeaturesZ = comFeaturesZ(sigFeatQ(:,i),:); % get just selected channels
+        fQv = vertcat(fQv,mean(sigComFeaturesZ(:,iF),2)); %average over fast trials
+        sQv = vertcat(sQv,mean(sigComFeaturesZ(:,iS),2)); %average over slow trials
+        aQv = vertcat(aQv,mean(sigComFeaturesZ(:,:),2)); %average over all trials
+        if ~isempty(sigComFeaturesZ)
+            fIDq = vertcat(fIDq,i*ones(height(sigComFeaturesZ),1)); % frequency band
+            ssIDq = vertcat(ssIDq,isubj*ones(height(sigComFeaturesZ),1)); % subject ID
+            gchQ = vertcat(gchQ,NFstructQ(isubj).gchlbl(sigFeatQ(:,i))); % channel label
+        end
+    end
+    clear nComDat nPowDat
+end
+
+% save details about selected features
+
+save([savedir2 savefname2],'fPv','sPv','fIDp','ssIDp','gchP','fQv','sQv','aQv','aPv','fIDq','ssIDq','gchQ')
